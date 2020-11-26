9599b0e200d187cf53f313763a08530ca679b278
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2020-11-26 16:50:05 +0100

    Add folder examples

---
+++ examples/lambda_folder/main.tf
@@ -0,0 +1,34 @@
+provider "grafana" {
+  url  = "http://grafana:3000"
+  auth = "admin:admin"
+}
+
+variable "name" {
+  type = string
+}
+
+resource "grafana_folder" "this" {
+  title = "${var.name}-folder"
+}
+
+module "dashboard" {
+
+  source = "../../"
+
+  enable = true
+  dashboard_configuration = {
+    name        = var.name
+    environment = "prod"
+    data_source = "prod"
+    alert       = false
+    service     = "lambda"
+    folder      = grafana_folder.this.id
+  }
+  lambda_configuration = {
+    trigger = "cron"
+  }
+}
+
+output "op" {
+  value = module.dashboard
+}
