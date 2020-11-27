1d3b7e9a6657e7201d054c87c8446e1265cf21a9
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2020-11-27 16:45:57 +0100

    add nonotation tests

--- test/unit/__init__.py
+++ test/unit/__init__.py
@@ -1,11 +1,11 @@
-'''
+"""
 tests module
-'''
+"""
 
 import os
 import sys
 
 import sure
 
-ROOT_DIR = os.path.join(os.path.dirname(__file__), '../..')
+ROOT_DIR = os.path.join(os.path.dirname(__file__), "../..")
 sys.path.append(ROOT_DIR)
--- test/unit/test_bin.py
+++ test/unit/test_bin.py
@@ -2,27 +2,17 @@ from argparse import Namespace
 
 from bin import apply_options
 
-class TestApplyOptions():
-  """Test parse_option
-  """
 
-  def test_should_generate_notifications_object(self):
-    """should generate proper notifications object when notifications is present
-    """
+class TestApplyOptions:
+    """Test parse_option"""
 
-    cli_options = Namespace(notifications=["slack-1", "slack-2"])
+    def test_should_generate_notifications_object(self):
+        """should generate proper notifications object when notifications is present"""
 
-    expected_notifications_block = [
-      {
-        "uid": "slack-1"
-      },
-      {
-        "uid": "slack-2"
-      }
-    ]
+        cli_options = Namespace(notifications=["slack-1", "slack-2"])
 
-    expected_args = Namespace(notifications=expected_notifications_block)
+        expected_notifications_block = [{"uid": "slack-1"}, {"uid": "slack-2"}]
 
-    apply_options.when.called_with(cli_options).should.return_value(expected_args)
+        expected_args = Namespace(notifications=expected_notifications_block)
 
-
+        apply_options.when.called_with(cli_options).should.return_value(expected_args)
