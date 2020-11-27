48dad19655dd5e53ace5dded26cd145b396a479c
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2020-11-27 22:38:50 +0100

    lambda unit tests

---
+++ test/unit/test_lambdas.py
@@ -0,0 +1,16 @@
+from lib.lambdas import dispatcher
+
+import re
+
+
+class TestDispatcher:
+    def test_should_throw_exception_when_dispatcher_called_with_wrong_arguments(self):
+        dispatcher.when.called_with(service="foo", trigger="bar").should.throw(
+            Exception, r"dispatcher recieved a non l"
+        )
+
+    def test_should_call_trigger_handlers(self):
+        expected_triggers = ["cognito", "cron", "events", "logs", "sns", "sqs"]
+
+        for trigger in expected_triggers:
+            dispatcher.when.called_with(service="lambda", trigger=trigger).should.be.ok
