cec2d17430e6a56f8505f0e7f94d375e24008f83
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2020-12-15 13:07:54 +0100

    nits

--- lib/commons.py
+++ lib/commons.py
@@ -10,3 +10,16 @@ RETENTION_POLICY = "autogen"
 
 def get_documentation_link(url: str):
     return {"targetBlank": True, "title": "Link to Documentation", "url": url}
+
+
+def get_series_overrides(min_alias: str, mean_alias: str, max_alias: str):
+    return [
+        {"alias": min_alias, "color": "#C8F2C2", "lines": False},
+        {"alias": mean_alias, "color": "#FADE2A", "fill": 0},
+        {
+            "alias": max_alias,
+            "color": "rgb(77, 159, 179)",
+            "fillBelowTo": min_alias,
+            "lines": False,
+        },
+    ]
--- lib/rds.py
+++ lib/rds.py
@@ -9,6 +9,7 @@ from grafanalib.core import (
     Dashboard,
     Graph,
     GreaterThan,
+    LowerThan,
     # MILLISECONDS_FORMAT,
     OP_AND,
     PERCENT_FORMAT,
@@ -29,6 +30,7 @@ from lib.commons import (
     ALERT_REF_ID,
     EDITABLE,
     get_documentation_link,
+    get_series_overrides,
     RAW_QUERY,
     RETENTION_POLICY,
     SHARED_CROSSHAIR,
@@ -75,16 +77,7 @@ def generate_rds_cpu_graph(name: str, data_source: str, notifications: List[str]
         ),
     ]
 
-    series_overrides = [
-        {"alias": min_alias, "color": "#C8F2C2", "lines": False},
-        {"alias": mean_alias, "color": "#FADE2A", "fill": 0},
-        {
-            "alias": max_alias,
-            "color": "rgb(77, 159, 179)",
-            "fillBelowTo": min_alias,
-            "lines": False,
-        },
-    ]
+    series_overrides = get_series_overrides(min_alias, mean_alias, max_alias)
 
     alert = None
 
@@ -121,6 +114,59 @@ def generate_rds_cpu_graph(name: str, data_source: str, notifications: List[str]
     ).auto_ref_ids()
 
 
+def generate_rds_burst_balance_graph(
+    name: str, data_source: str, notifications: List[str]
+):
+    """
+    Generate rds graph
+    """
+
+    y_axes = single_y_axis(format=PERCENT_FORMAT)
+
+    targets = [
+        InfluxDBTarget(
+            query='SELECT min("burst_balance_minimum") FROM "{}"."{}" WHERE ("db_instance_identifier" =\'{}\') AND $timeFilter GROUP BY time(1m) fill(previous)'.format(
+                RETENTION_POLICY, RDS_MEASUREMENT, name
+            ),
+            rawQuery=RAW_QUERY,
+            refId=ALERT_REF_ID,
+        ),
+    ]
+
+    alert = None
+
+    if notifications:
+        alert = Alert(
+            name="{} Burst Balance Errors".format(name),
+            message="{} is having Burst Balance errors".format(name),
+            executionErrorState="alerting",
+            noDataState="keep_state",
+            alertConditions=[
+                AlertCondition(
+                    Target(refId=ALERT_REF_ID),
+                    timeRange=TimeRange("5m", "now"),
+                    evaluator=LowerThan(40),
+                    reducerType=RTYPE_MAX,
+                    operator=OP_AND,
+                )
+            ],
+            gracePeriod="1m",
+            notifications=notifications,
+        )
+
+    return Graph(
+        title="Burst Balance",
+        dataSource=data_source,
+        targets=targets,
+        yAxes=y_axes,
+        transparent=TRANSPARENT,
+        editable=EDITABLE,
+        bars=False,
+        lines=True,
+        alert=alert,
+    ).auto_ref_ids()
+
+
 def generate_rds_dashboard(
     name: str,
     environment: str,
@@ -135,6 +181,9 @@ def generate_rds_dashboard(
     cpu_graph = generate_rds_cpu_graph(
         name=name, data_source=data_source, notifications=notifications
     )
+    burst_graph = generate_rds_burst_balance_graph(
+        name=name, data_source=data_source, notifications=notifications
+    )
 
     return Dashboard(
         title="RDS: {}".format(name),
@@ -144,7 +193,7 @@ def generate_rds_dashboard(
         tags=tags,
         timezone=TIMEZONE,
         sharedCrosshair=SHARED_CROSSHAIR,
-        rows=[Row(panels=[cpu_graph])],
+        rows=[Row(panels=[cpu_graph, burst_graph])],
         links=[
             get_documentation_link(
                 "https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/MonitoringOverview.html"
