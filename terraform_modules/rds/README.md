9d93e6099deaa87deca8991060ff90a5c4bb9669
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2020-12-16 16:02:00 +0100

    Add RDS TF module + tests

---
+++ terraform_modules/rds/README.md
@@ -0,0 +1,24 @@
+## Requirements
+
+No requirements.
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| grafana | n/a |
+| local | n/a |
+| null | n/a |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| enable | true to enable the module | `bool` | `false` | no |
+| grafana\_configuration | Configuration for creating Grafana dashboards and alerts | <pre>object({<br>    name          = string<br>    environment   = string<br>    data_source   = string<br>    folder        = string<br>    engine        = string<br>    notifications = list(string)<br>  })</pre> | n/a | yes |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| output | Grafana slug and dashboard\_id |
---
+++ test/integration/rds_grafana_test.go
@@ -0,0 +1,51 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRDS_noDashboard(t *testing.T) {
+	// t.Parallel()
+	dashboardName := fmt.Sprintf("rds-%s", random.UniqueId())
+	exampleDir := "../../examples/rds_dashboards/no_dashboard/"
+
+	terraformOptions := SetupExample(t, dashboardName, exampleDir)
+	t.Logf("Terraform module inputs: %+v", *terraformOptions)
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraformApplyOutput := terraform.InitAndApply(t, terraformOptions)
+	resourceCount := terraform.GetResourceCount(t, terraformApplyOutput)
+
+	require.Equal(t, resourceCount.Add, 0)
+	require.Equal(t, resourceCount.Change, 0)
+	require.Equal(t, resourceCount.Destroy, 0)
+}
+
+func TestRDS_dashboard(t *testing.T) {
+	// t.Parallel()
+	dashboardName := fmt.Sprintf("rds-%s", random.UniqueId())
+	exampleDir := "../../examples/rds_dashboards/dashboard/"
+
+	terraformOptions := SetupExample(t, dashboardName, exampleDir)
+	t.Logf("Terraform module inputs: %+v", *terraformOptions)
+	defer terraform.Destroy(t, terraformOptions)
+
+	TerraformApplyAndValidateOutputs(t, terraformOptions)
+}
+
+func TestRDS_folder(t *testing.T) {
+	// t.Parallel()
+	dashboardName := fmt.Sprintf("rds-%s", random.UniqueId())
+	exampleDir := "../../examples/rds_dashboards/dashboard_with_folder/"
+
+	terraformOptions := SetupExample(t, dashboardName, exampleDir)
+	t.Logf("Terraform module inputs: %+v", *terraformOptions)
+	defer terraform.Destroy(t, terraformOptions)
+
+	TerraformApplyAndValidateOutputs(t, terraformOptions)
+}
