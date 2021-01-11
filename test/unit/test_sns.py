b7db27217bd233be33967702b3ea93878e35bdf7
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2021-01-11 19:59:38 +0530

    fix SNS tests

--- test/unit/test_sns.py
+++ test/unit/test_sns.py
@@ -1,5 +1,5 @@
 from grafanalib.core import Alert, AlertCondition, Graph, Target
-from grafanalib.influxdb import InfluxDBTarget
+from grafanalib.cloudwatch import CloudwatchMetricsTarget
 from lib.sns import create_sns_graph
 
 
@@ -10,30 +10,27 @@ class TestCreateSNSGraph:
         expected_topic_name = "sns-1"
         expected_title = "SNS: sns-1"
         expected_notifications = ["slack-1"]
-        expected_queries = [
-            'SELECT sum("number_of_messages_published_sum") FROM "autogen"."cloudwatch_aws_sns" WHERE ("topic_name" = \'{}\') AND $timeFilter GROUP BY time(5m) fill(0)'.format(
-                expected_topic_name
+        expected_targets = [
+            CloudwatchMetricsTarget(
+                statistics=["Sum"],
+                metricName="NumberOfMessagesPublished",
+                dimensions={"TopicName": expected_topic_name},
             ),
-            'SELECT sum("number_of_notifications_delivered_sum") FROM "autogen"."cloudwatch_aws_sns" WHERE ("topic_name" = \'{}\') AND $timeFilter GROUP BY time(5m) fill(0)'.format(
-                expected_topic_name
+            CloudwatchMetricsTarget(
+                statistics=["Sum"],
+                metricName="NumberOfNotificationsDelivered",
+                dimensions={"TopicName": expected_topic_name},
             ),
-            'SELECT sum("number_of_notifications_failed_sum") FROM "autogen"."cloudwatch_aws_sns" WHERE ("topic_name" = \'{}\') AND $timeFilter GROUP BY time(5m) fill(0)'.format(
-                expected_topic_name
+            CloudwatchMetricsTarget(
+                statistics=["Sum"],
+                metricName="NumberOfMessagesFailed",
+                dimensions={"TopicName": expected_topic_name},
             ),
         ]
 
-        expected_alert_query = InfluxDBTarget(
-            alias="Failed",
-            query='SELECT sum("number_of_notifications_failed_sum") FROM "autogen"."cloudwatch_aws_sns" WHERE ("topic_name" = \'{}\') AND $timeFilter GROUP BY time(5m) fill(0)'.format(
-                expected_topic_name
-            ),
-            rawQuery=True,
-            refId="A",
-        )
-
         actual_sns_graph = create_sns_graph(
             name=expected_topic_name,
-            data_source=expected_data_source,
+            cloudwatch_data_source=expected_data_source,
             notifications=expected_notifications,
         )
         actual_sns_graph.should.be.a(Graph)
@@ -63,10 +60,20 @@ class TestCreateSNSGraph:
         actual_sns_graph.should.have.property("targets").length_of(3)
 
         for target in actual_sns_graph.targets:
-            target.should.be.a(InfluxDBTarget)
-            target.query.should.be.within(expected_queries)
+            target.should.be.a(CloudwatchMetricsTarget)
+            target.namespace.should.be.equal("AWS/SNS")
+            target.period.should.be.equal("5m")
+            target.statistics.should.be.equal(["Sum"])
+            target.dimensions.should.be.equal({"TopicName": expected_topic_name})
+            target.metricName.should.be.within(
+                [
+                    "NumberOfMessagesPublished",
+                    "NumberOfNotificationsDelivered",
+                    "NumberOfMessagesFailed",
+                ]
+            )
 
-        actual_sns_graph.targets.should.contain(expected_alert_query)
+        actual_sns_graph.targets[2].refId.should.be.equal("A")
 
     def test_should_generate_correct_object_with_no_alert_notifications(self):
 
@@ -76,7 +83,7 @@ class TestCreateSNSGraph:
 
         actual_sns_graph = create_sns_graph(
             name=expected_topic_name,
-            data_source=expected_data_source,
+            cloudwatch_data_source=expected_data_source,
             notifications=expected_notifications,
         )
         actual_sns_graph.should.have.property("alert").with_value.equal(None)
@@ -89,61 +96,14 @@ class TestCreateSNSGraph:
         )
         expected_topic_name = "lambda-elasticsearch-booking-alpha"
         expected_title = "SNS: lambda-elasticsearch-booking-alpha"
-        expected_notifications = ["slack-1"]
-        expected_queries = [
-            'SELECT sum("number_of_messages_published_sum") FROM "autogen"."cloudwatch_aws_sns" WHERE ("topic_name" = \'{}\') AND $timeFilter GROUP BY time(5m) fill(0)'.format(
-                expected_topic_name
-            ),
-            'SELECT sum("number_of_notifications_delivered_sum") FROM "autogen"."cloudwatch_aws_sns" WHERE ("topic_name" = \'{}\') AND $timeFilter GROUP BY time(5m) fill(0)'.format(
-                expected_topic_name
-            ),
-            'SELECT sum("number_of_notifications_failed_sum") FROM "autogen"."cloudwatch_aws_sns" WHERE ("topic_name" = \'{}\') AND $timeFilter GROUP BY time(5m) fill(0)'.format(
-                expected_topic_name
-            ),
-        ]
-
-        expected_alert_query = InfluxDBTarget(
-            alias="Failed",
-            query='SELECT sum("number_of_notifications_failed_sum") FROM "autogen"."cloudwatch_aws_sns" WHERE ("topic_name" = \'{}\') AND $timeFilter GROUP BY time(5m) fill(0)'.format(
-                expected_topic_name
-            ),
-            rawQuery=True,
-            refId="A",
-        )
 
         actual_sns_graph = create_sns_graph(
             name=sns_topic_arn,
-            data_source=expected_data_source,
-            notifications=expected_notifications,
+            cloudwatch_data_source=expected_data_source,
+            notifications=[],
         )
         actual_sns_graph.should.be.a(Graph)
         actual_sns_graph.should.have.property("title").with_value.equal(expected_title)
-        actual_sns_graph.should.have.property("bars").with_value.equal(True)
-        actual_sns_graph.should.have.property("lines").with_value.equal(False)
-
-        actual_sns_graph.should.have.property("alert").a(Alert)
-        actual_sns_graph.alert.should.have.property(
-            "executionErrorState"
-        ).which.should.be.equal("alerting")
-        actual_sns_graph.alert.should.have.property(
-            "noDataState"
-        ).which.should.be.equal("keep_state")
-        actual_sns_graph.alert.should.have.property(
-            "notifications"
-        ).which.should.be.equal(expected_notifications)
-        actual_sns_graph.alert.should.have.property(
-            "alertConditions"
-        ).which.should.have.length_of(1)
-        actual_sns_graph.alert.alertConditions[0].should.be.a(AlertCondition)
-        actual_sns_graph.alert.alertConditions[0].should.have.property("target")
-        actual_sns_graph.alert.alertConditions[0].target.should.be.equal(
-            Target(refId="A")
-        )
-
-        actual_sns_graph.should.have.property("targets").length_of(3)
-
+        actual_sns_graph.targets.should.have.length_of(3)
         for target in actual_sns_graph.targets:
-            target.should.be.a(InfluxDBTarget)
-            target.query.should.be.within(expected_queries)
-
-        actual_sns_graph.targets.should.contain(expected_alert_query)
+            target.dimensions.should.equal({"TopicName": expected_topic_name})
