0fb889e938da9a9a94d57855084c864ba3726474
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2021-03-01 18:38:06 +0100

    TF 0.13 upgrade

---
+++ terraform_modules/firehose/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    grafana = {
+      source = "grafana/grafana"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+  }
+  required_version = ">= 0.13"
+}
