7234401b112dd352eb3c9a11014c5ab5277a1186
1337blue <phillip.lichtblau@gmx.de>
2021-04-15 14:41:18 +0200

    tf nits

--- terraform_modules/elasticache_redis/main.tf
+++ terraform_modules/elasticache_redis/main.tf
@@ -20,7 +20,9 @@ locals {
   dahboard_path           = "${path.module}/dashboard.json"
   alert_dahboard_path     = "${path.module}/alert_dashboard.json"
   create_alerts_dashboard = length(var.grafana_configuration.notifications) > 0 ? true : false
-  notification_args       = length(var.grafana_configuration.notifications) > 0 ? "--notifications ${join(" ", var.grafana_configuration.notifications)}" : ""
+  notification_args = length(var.grafana_configuration.notifications) > 0 ? "--notifications ${join(
+    " ", var.grafana_configuration.notifications
+  )}" : ""
 }
 
 resource "null_resource" "generate_dashboard" {
@@ -28,7 +30,15 @@ resource "null_resource" "generate_dashboard" {
   count = var.enable ? 1 : 0
 
   provisioner "local-exec" {
-    command = "python3 ${path.module}/../../bin.py --name ${var.grafana_configuration.name} --environment ${var.grafana_configuration.environment} --cloudwatch_data_source ${var.grafana_configuration.cloudwatch_data_source} --influxdb_data_source ${var.grafana_configuration.influxdb_data_source} elasticsearch --client_id ${var.grafana_configuration.client_id} | json_pp > ${local.dahboard_path}"
+    command = join(" ", [
+      "python3 ${path.module}/../../bin.py",
+      "--name ${var.grafana_configuration.name}",
+      "--environment ${var.grafana_configuration.environment}",
+      "--cloudwatch_data_source ${var.grafana_configuration.cloudwatch_data_source}",
+      "--influxdb_data_source ${var.grafana_configuration.influxdb_data_source} elasticache-redis",
+      "--client_id ${var.grafana_configuration.client_id}",
+      "| json_pp > ${local.dahboard_path}"
+    ])
   }
 
   triggers = {
