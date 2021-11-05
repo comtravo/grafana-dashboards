b9e14711194e3015adf34849e5dd1693dadd34d4
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2021-11-05 16:01:18 +0100

    nits

--- test/unit/test_api_gateways.py
+++ test/unit/test_api_gateways.py
@@ -132,28 +132,28 @@ class TestAPIGatewayDashboards:
         generated_dashboard.annotations.should.be.a(Annotations)
         generated_dashboard.templating.should.be.a(Templating)
         generated_dashboard.tags.should.have.length_of(2)
-        generated_dashboard.rows.should.have.length_of(2)
+        generated_dashboard.rows.should.have.length_of(1)
 
-    # def test_should_generate_proper_dashboard_with_lambdas(self):
-    #     apig_name = "apig-1"
-    #     cloudwatch_data_source = "prod"
-    #     influxdb_data_source = "influxdb"
-    #     environment = "prod"
-    #     notifications = ["foo-1", "foo-2"]
-    #     lambdas = ["lambda-1", "lambda-2"]
+    def test_should_generate_proper_dashboard_with_lambdas(self):
+        apig_name = "apig-1"
+        cloudwatch_data_source = "prod"
+        lambda_insights_namespace = "influxdb"
+        environment = "prod"
+        notifications = ["foo-1", "foo-2"]
+        lambdas = ["lambda-1", "lambda-2"]
 
-    #     generated_dashboard = generate_api_gateways_dashboard(
-    #         name=apig_name,
-    #         cloudwatch_data_source=cloudwatch_data_source,
-    #         influxdb_data_source=influxdb_data_source,
-    #         notifications=notifications,
-    #         environment=environment,
-    #         lambdas=lambdas,
-    #     )
+        generated_dashboard = generate_api_gateways_dashboard(
+            name=apig_name,
+            cloudwatch_data_source=cloudwatch_data_source,
+            lambda_insights_namespace=lambda_insights_namespace,
+            notifications=notifications,
+            environment=environment,
+            lambdas=lambdas,
+        )
 
-    #     generated_dashboard.should.be.a(Dashboard)
-    #     generated_dashboard.title.should.match(r"API Gateway:")
-    #     generated_dashboard.annotations.should.be.a(Annotations)
-    #     generated_dashboard.templating.should.be.a(Templating)
-    #     generated_dashboard.tags.should.have.length_of(3)
-    #     generated_dashboard.rows.should.have.length_of(3)
+        generated_dashboard.should.be.a(Dashboard)
+        generated_dashboard.title.should.match(r"API Gateway:")
+        generated_dashboard.annotations.should.be.a(Annotations)
+        generated_dashboard.templating.should.be.a(Templating)
+        generated_dashboard.tags.should.have.length_of(3)
+        generated_dashboard.rows.should.have.length_of((len(lambdas) * 2) + 2)
