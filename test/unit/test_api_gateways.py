b11a801c6fddce23f22c8d2763099253dbc32409
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2021-11-05 15:58:32 +0100

    nits

--- test/unit/test_api_gateways.py
+++ test/unit/test_api_gateways.py
@@ -110,67 +110,29 @@ class TestAPIGatewayDashboards:
         generated_graph.alert.alertConditions[0].target.should.equal(Target(refId="A"))
         generated_graph.targets[0].should.eql(targets[0])
 
-    # def test_should_generate_proper_requests_4xx_graph(self):
-    #     apig_name = "apig-1"
-    #     cloudwatch_data_source = "prod"
-    #     influxdb_data_source = "influxdb"
+    def test_should_generate_proper_dashboard(self):
+        apig_name = "apig-1"
+        cloudwatch_data_source = "prod"
+        lambda_insights_namespace = "influxdb"
+        environment = "prod"
+        notifications = ["foo-1", "foo-2"]
+        lambdas = []
 
-    #     targets = [
-    #         CloudwatchMetricsTarget(
-    #             alias="4xx",
-    #             namespace="AWS/ApiGateway",
-    #             period="1m",
-    #             statistics=["Sum"],
-    #             metricName="4XXError",
-    #             dimensions={"ApiName": apig_name},
-    #             refId="A",
-    #         ),
-    #         CloudwatchMetricsTarget(
-    #             alias="requests",
-    #             namespace="AWS/ApiGateway",
-    #             dimensions={"ApiName": apig_name},
-    #             period="1m",
-    #             statistics=["Sum"],
-    #             metricName="Count",
-    #             refId="B",
-    #         ),
-    #     ]
+        generated_dashboard = generate_api_gateways_dashboard(
+            name=apig_name,
+            cloudwatch_data_source=cloudwatch_data_source,
+            lambda_insights_namespace=lambda_insights_namespace,
+            notifications=notifications,
+            environment=environment,
+            lambdas=lambdas,
+        )
 
-    #     generated_graph = generate_api_gateway_requests_4xx_graph(
-    #         name=apig_name,
-    #         cloudwatch_data_source=cloudwatch_data_source,
-    #         influxdb_data_source=influxdb_data_source,
-    #     )
-    #     generated_graph.should.be.a(Graph)
-    #     generated_graph.title.should.match(r"API Gateway Requests and 4XX errors")
-    #     generated_graph.dataSource.should.eql(cloudwatch_data_source)
-    #     generated_graph.alert.should.equal(None)
-    #     generated_graph.targets.should.have.length_of(2)
-    #     generated_graph.targets.should.equal(targets)
-
-    # def test_should_generate_proper_dashboard(self):
-    #     apig_name = "apig-1"
-    #     cloudwatch_data_source = "prod"
-    #     influxdb_data_source = "influxdb"
-    #     environment = "prod"
-    #     notifications = ["foo-1", "foo-2"]
-    #     lambdas = []
-
-    #     generated_dashboard = generate_api_gateways_dashboard(
-    #         name=apig_name,
-    #         cloudwatch_data_source=cloudwatch_data_source,
-    #         influxdb_data_source=influxdb_data_source,
-    #         notifications=notifications,
-    #         environment=environment,
-    #         lambdas=lambdas,
-    #     )
-
-    #     generated_dashboard.should.be.a(Dashboard)
-    #     generated_dashboard.title.should.match(r"API Gateway:")
-    #     generated_dashboard.annotations.should.be.a(Annotations)
-    #     generated_dashboard.templating.should.be.a(Templating)
-    #     generated_dashboard.tags.should.have.length_of(2)
-    #     generated_dashboard.rows.should.have.length_of(2)
+        generated_dashboard.should.be.a(Dashboard)
+        generated_dashboard.title.should.match(r"API Gateway:")
+        generated_dashboard.annotations.should.be.a(Annotations)
+        generated_dashboard.templating.should.be.a(Templating)
+        generated_dashboard.tags.should.have.length_of(2)
+        generated_dashboard.rows.should.have.length_of(2)
 
     # def test_should_generate_proper_dashboard_with_lambdas(self):
     #     apig_name = "apig-1"
