5a37a1027d9a664395c04ddb9b1a108892ab844a
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2021-11-15 11:36:19 +0100

    nits

--- lib/ecs.py
+++ lib/ecs.py
@@ -6,6 +6,7 @@ from grafanalib.core import (
     Alert,
     AlertCondition,
     Dashboard,
+    Graph,
     GreaterThan,
     Logs,
     Row,
@@ -23,12 +24,16 @@ from lib.commons import (
 )
 
 from grafanalib.cloudwatch import CloudwatchMetricsTarget
-# from lib import colors
+from lib import colors
 
 from typing import List
 
 ECS_NAMESPACE = "AWS/ECS"
 CONTAINER_INSIGHTS_NAMESPACE = "ECS/ContainerInsights"
+
+MINIMUM_ALIAS = "Min"
+AVERAGE_ALIAS = "Avg"
+MAXIMUM_ALIAS = "Max"
 
 def generate_running_count_graph(
     name: str,
@@ -91,6 +96,71 @@ def generate_running_count_graph(
       ]
     )
 
+def generate_cpu_utilization_graph(
+    name: str,
+    cloudwatch_data_source: str,
+    cluster_name: str,
+    *args,
+    **kwargs
+) -> Stat:
+    """
+    Generate lambda graph
+    """
+
+    targets = [
+        CloudwatchMetricsTarget(
+            alias=MINIMUM_ALIAS,
+            namespace=ECS_NAMESPACE,
+            statistics=["Minimum"],
+            metricName="CPUUtilization",
+            dimensions={
+              "ServiceName": name,
+              "ClusterName": cluster_name
+              },
+        ),
+        CloudwatchMetricsTarget(
+            alias=AVERAGE_ALIAS,
+            namespace=ECS_NAMESPACE,
+            statistics=["Average"],
+            metricName="CPUUtilization",
+            dimensions={
+              "ServiceName": name,
+              "ClusterName": cluster_name
+              },
+        ),
+        CloudwatchMetricsTarget(
+            alias=MAXIMUM_ALIAS,
+            namespace=ECS_NAMESPACE,
+            statistics=["Maximum"],
+            metricName="CPUUtilization",
+            dimensions={
+              "ServiceName": name,
+              "ClusterName": cluster_name
+              },
+            refId=ALERT_REF_ID,
+        ),
+    ]
+
+    seriesOverrides = [
+        {"alias": MINIMUM_ALIAS, "color": "#C8F2C2", "lines": False},
+        {"alias": AVERAGE_ALIAS, "color": "#FADE2A", "fill": 0},
+        {
+            "alias": MAXIMUM_ALIAS,
+            "color": "rgb(77, 159, 179)",
+            "fillBelowTo": MINIMUM_ALIAS,
+            "lines": False,
+        },
+    ]
+
+    return Graph(
+        title="CPU utilization percentage",
+        dataSource=cloudwatch_data_source,
+        targets=targets,
+        seriesOverrides=seriesOverrides,
+        transparent=TRANSPARENT,
+        editable=EDITABLE,
+    ).auto_ref_ids()
+
 def generate_deployment_graph(
     name: str,
     cloudwatch_data_source: str,
@@ -143,6 +213,9 @@ def generate_ecs_service_dashboard(
     Row(title="Deployments",showTitle=True, collapse=False, panels=[
       running_count_graph,
       deployment_graph
+    ]),
+    Row(title="Utilization",showTitle=True, collapse=False, panels=[
+      generate_cpu_utilization_graph(name=name, cloudwatch_data_source=cloudwatch_data_source, *args, **kwargs)
     ])
   ]
   return Dashboard(
